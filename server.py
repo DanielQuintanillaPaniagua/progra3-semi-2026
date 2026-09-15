95fcc92949b3f08148808a127e25168c11db11a2
DanielQuintanillaPaniagua <danielquintanillapaniagua@gmail.com>
2026-09-14 19:48:25 -0600

    saludo

--- server.py
+++ server.py
@@ -1,16 +1,28 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
-from urllib import parse
+from urllib import parse 
 from urllib.parse import urlparse, parse_qs
+
 import json
 
 port = 3000
 
-class miservidor(SimpleHTTPRequestHandler):
+class miServidor(SimpleHTTPRequestHandler):
     def do_GET(self):
+        urlParse = urlparse(self.path)
+        qs = parse_qs(urlParse.query)
+       
+        if urlParse.path == "/saludo":
+            saludo = qs["nombre"][0] + " bienvenido a Python"
+            
+            self.send_response(200)
+            self.send_header("Content-type","text/html")
+            self.end_headers()
+            self.wfile.write(saludo.encode("utf-8"))
+
         if self.path == "/":
             self.path = "/index.html"
-        return SimpleHTTPRequestHandler.do_GET(self)
+            return SimpleHTTPRequestHandler.do_GET(self)
 
-print(f"servidor corriendo en el puerto {port}")
-server = HTTPServer(("localhost", port), miservidor)
+print(f"Servidor corriendo en el puerto {port}")
+server = HTTPServer(("localhost",port),miServidor)
 server.serve_forever()
